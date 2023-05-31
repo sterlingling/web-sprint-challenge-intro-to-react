4eda9627f0c9fb13b7530fadd5fcc4756f825235
Sterling Sanders <sterlingsanders723@gmail.com>
2023-05-30 17:47:19 -0700

    got it passing tests

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,18 @@
 // Write your Character component here
+import React from 'react';
+
+
+const Character = (props) => {
+
+    return (
+        < div className='character'>
+            <h1>{props.name}     {props.birth_year}</h1>
+            
+        </div >
+    )
+}
+
+export default Character
+// data.map((info) => (
+//     <h3>Name: {info.name}     Birth Year:{info.birth_year}</h3>
+// ))
